ad16527f35fc017f7466efa63e0a3bc7484cd13a
abautist <auggie.bautista@gmail.com>
2015-11-16 01:55:39 -0800

    lettering.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ var data = [
 {candidate: "Tie", value: 14}
 ];
 
-var width = 420,
-    barHeight = 20;
+var width = 820,
+    barHeight = 40;
 
 var x = d3.scale.linear()
     .range([0, width]);
@@ -18,23 +18,38 @@ var chart = d3.select(".chart")
 
   chart.attr("height", barHeight * data.length);
 
+
+
   var bar = chart.selectAll("g")
       .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    .enter()
+      .append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; }) 
 
   bar.append("rect")
       .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
+      .attr("height", barHeight - 1)
+      .on('mouseover', function() {
+        d3.select(this)
+          .style({"fill": "red"});
+      })
+      .on("mouseout", function() {
+        d3.select(this)
+          .style({"fill": "steelblue"});
+      });
 
   bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("x", 8)
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
-      .text(function(d) { return d.value; });
+      .text(function(d) { return d.candidate + " " + (d.value+"%"); })
 
 function type(d) {
   d.value = +d.value; // coerce to number
   return d;
 }
+
+$(document).ready(function() {
+    $(".fancy_title").lettering();
+});
 
